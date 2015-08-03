211fe602cecbc683c9821fda3036ebf593d18d4e
Hugo Miranda Mac <hugo@aioninnovations.com>
2015-08-02 19:53:06 -0700

    add initial cli features

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"strconv"
+)
 
-func main() {
+type intslice []int
 
-	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	processed := process_list(slice, 0)
-	place := find_safer_place(processed, 0)
+var mylist intslice
 
-	fmt.Println(place)
+func (i *intslice) String() string {
+	return fmt.Sprintf("%d", *i)
+}
+
+func (i *intslice) Set(value string) error {
+
+	val, err := strconv.Atoi(value)
+	if err != nil {
+		*i = append(*i, -1)
+	} else {
+		*i = append(*i, val)
+	}
+
+	return nil
 
 }
 
@@ -50,5 +66,22 @@ func find_safer_place(slice []int, turn_start_cut_at int) int {
 	} else {
 		return find_safer_place(new_slice, 0)
 	}
+
+}
+func run(slice []int, pos int, out io.Writer) {
+	//slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// processed := process_list(slice, 0)
+	// place := find_safer_place(processed, 0)
+
+	fmt.Fprintf(out, "hahah")
+}
+
+func main() {
+
+	flag.Var(&mylist, "i", "List of items")
+	position := flag.Int("position", 0, "Set the position")
+	flag.Parse()
+
+	fmt.Println(mylist, *position)
 
 }
